ae17fc0d2c3c5c732c9916c01150a2acc204b35e
Wielopol <karolwiel@interia.pl>
2022-05-13 16:34:04 +0200

    Add custom query methods

--- src/main/java/com/sda/conferenceroomreservationsystem/repository/OrganizationRepository.java
+++ src/main/java/com/sda/conferenceroomreservationsystem/repository/OrganizationRepository.java
@@ -1,9 +1,13 @@
 package com.sda.conferenceroomreservationsystem.repository;
 
-import com.sda.conferenceroomreservationsystem.model.Organization;
+import com.sda.conferenceroomreservationsystem.model.entity.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+    Optional<Organization> findByOrganizationName(String organizationName);
+    Optional<Organization> findByOrganizationNameAndPassword(String organizationName, String password);
 }
