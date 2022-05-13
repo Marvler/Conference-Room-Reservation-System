7afc703d0d64c83118793fbaba7a741d3b3cde99
Wielopol <karolwiel@interia.pl>
2022-05-13 17:02:59 +0200

    Create mappers

---
+++ src/main/java/com/sda/conferenceroomreservationsystem/mapper/ConferenceRoomMapper.java
@@ -0,0 +1,30 @@
+package com.sda.conferenceroomreservationsystem.mapper;
+
+import com.sda.conferenceroomreservationsystem.model.dto.ConferenceRoomDto;
+import com.sda.conferenceroomreservationsystem.model.entity.ConferenceRoom;
+import com.sda.conferenceroomreservationsystem.model.request.ConferenceRoomRequest;
+
+public class ConferenceRoomMapper {
+    public ConferenceRoom map(final ConferenceRoomRequest request) {
+        final ConferenceRoom conferenceRoom = new ConferenceRoom();
+        conferenceRoom.setConferenceRoomName(request.getConferenceRoomName());
+        conferenceRoom.setConferenceRoomIdentifier(request.getConferenceRoomIdentifier());
+        conferenceRoom.setLevel(request.getLevel());
+        conferenceRoom.setNumberOfSeats(request.getNumberOfSeats());
+        conferenceRoom.setNumberOfStandings(request.getNumberOfStandings());
+
+        return conferenceRoom;
+    }
+
+    public ConferenceRoomDto map(final ConferenceRoom conferenceRoom) {
+        return ConferenceRoomDto.Builder()
+                .withConferenceRoomName(conferenceRoom.getConferenceRoomName())
+                .withConferenceRoomIdentifier(conferenceRoom.getConferenceRoomIdentifier())
+                .withLevel(conferenceRoom.getLevel())
+                .withAvailability(conferenceRoom.getAvailability())
+                .withNumberOfSeats(conferenceRoom.getNumberOfSeats())
+                .withNumberOfStandings(conferenceRoom.getNumberOfStandings())
+                .withReservations(conferenceRoom.getReservations())
+                .build();
+    }
+}
---
+++ src/main/java/com/sda/conferenceroomreservationsystem/mapper/OrganizationMapper.java
@@ -0,0 +1,25 @@
+package com.sda.conferenceroomreservationsystem.mapper;
+
+import com.sda.conferenceroomreservationsystem.model.dto.OrganizationDto;
+import com.sda.conferenceroomreservationsystem.model.entity.Organization;
+import com.sda.conferenceroomreservationsystem.model.request.OrganizationRequest;
+
+public class OrganizationMapper {
+
+    public Organization map(final OrganizationRequest request) {
+        final Organization organization = new Organization();
+        organization.setOrganizationName(request.getOrganizationName());
+        organization.setPassword(request.getPassword());
+        organization.setEmail(request.getEmail());
+
+        return organization;
+    }
+
+    public OrganizationDto map(final Organization organization) {
+        return OrganizationDto.Builder()
+                .withOrganizationName(organization.getOrganizationName())
+                .withEmail(organization.getEmail())
+                .withConferenceRooms(organization.getConferenceRooms())
+                .build();
+    }
+}
