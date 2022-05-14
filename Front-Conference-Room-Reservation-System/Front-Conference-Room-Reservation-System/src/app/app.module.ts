d78a29b4ba26bf0bdc2954374e6d2f38473368f5
Kamil D <kamil.dr91@gmail.com>
2022-05-14 14:53:23 +0200

    Feature-CRRS-2 Not found page added

--- Front-Conference-Room-Reservation-System/Front-Conference-Room-Reservation-System/src/app/app.module.ts
+++ Front-Conference-Room-Reservation-System/Front-Conference-Room-Reservation-System/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { OrganizationComponent } from './component/organization/organization.component';
+import { ReservationComponent } from './component/reservation/reservation.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
 
 @NgModule({
@@ -11,9 +14,12 @@ import { OrganizationComponent } from './component/organization/organization.com
     AppComponent,
     LoginComponent,
     OrganizationComponent,
+    ReservationComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
 
   ],
   providers: [],
