7590dd827787d96c3ee1c6e7c6dfdb8168bd1c27
Wielopol <karolwiel@interia.pl>
2022-05-17 01:45:54 +0200

    Add feature for reservation which collides with other reservations

--- Conference-Room-Reservation-System/Conference-Room-Reservation-System/src/main/java/com/sda/conferenceroomreservationsystem/model/request/ReservationRequest.java
+++ Conference-Room-Reservation-System/Conference-Room-Reservation-System/src/main/java/com/sda/conferenceroomreservationsystem/model/request/ReservationRequest.java
@@ -1,9 +1,11 @@
 package com.sda.conferenceroomreservationsystem.model.request;
 
+import com.sda.conferenceroomreservationsystem.model.entity.Reservation;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class ReservationRequest {
@@ -13,4 +15,17 @@ public class ReservationRequest {
     private LocalDateTime reservationEnd;
     @NotNull
     private Long conferenceRoomId;
+
+    public boolean isOccupied(List<Reservation> reservations) {
+        boolean isOccupied = false;
+
+        for (Reservation r : reservations) {
+            if (r.getReservationStart().isBefore(reservationEnd.minusSeconds(1)) && !r.getReservationEnd().isBefore(reservationStart.plusSeconds(1)) ||
+                    r.getReservationEnd().isAfter(reservationStart.plusSeconds(1)) && !r.getReservationStart().isAfter(reservationEnd.minusSeconds(1))) {
+                isOccupied = true;
+            }
+        }
+
+        return isOccupied;
+    }
 }
